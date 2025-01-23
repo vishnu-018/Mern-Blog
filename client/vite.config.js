a7093b069b850c89854bae7e2f8e0cb2a3fc542a
vishnu-018 <vishnu.ec21@bitsathy.ac.in>
2025-01-23 16:43:24 +0530

    create pages and routes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-plugins: [react()],
+  plugins: [react()],
   css: {
-   postcss: {
-    plugins: [tailwindcss()],
-   },
+    postcss: './postcss.config.js',
   },
 });
