51cb5980e1ff94b31431f72f2b9b4bca22b32e80
vishnu-018 <vishnu.ec21@bitsathy.ac.in>
2025-01-26 10:54:51 +0530

    create signup API route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 // Correctly format the MongoDB connection string and call mongoose.connect
@@ -14,9 +15,11 @@ mongoose.connect(process.env.MONGO)
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
