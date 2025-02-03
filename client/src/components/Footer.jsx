575c517c08480a09de03507a80dd9db8f66821a6
vishnu-018 <vishnu.ec21@bitsathy.ac.in>
2025-02-03 09:57:41 +0530

    create and add footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,76 @@
+import { Footer } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
+
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto p-6">
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-8">
+          {/* Logo Section */}
+          <div className="mb-6">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Inspire
+              </span>{' '}
+              Blog
+            </Link>
+          </div>
+
+          {/* Links Section */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.100jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  100 JS Projects
+                </Footer.Link>
+                <Footer.Link href="/about" target="_blank" rel="noopener noreferrer">
+                  Inspire Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="https://www.github.com/vishnu-018" target="_blank" rel="noopener noreferrer">
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+
+        {/* Divider */}
+        <Footer.Divider />
+
+        {/* Bottom Section */}
+        <div className="w-full flex flex-col sm:flex-row items-center justify-between mt-4">
+          <Footer.Copyright href="#" by=" Inspire Blog" year={new Date().getFullYear()} />
+          <div className="flex gap-6 mt-4 sm:mt-0">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="https://github.com/vishnu-018" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
