2f52ec22ddc0792fb0ba802c83b6b9ea1ba6d939
vishnu-018 <vishnu.ec21@bitsathy.ac.in>
2025-02-04 09:39:23 +0530

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,12 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link,useNavigate } from "react-router-dom";
-
+import { useDispatch ,useSelector} from "react-redux";
+import { signInSuccess,signInStart,signInFailure } from "../redux/user/userSlice";
 export default function SignIn() {
   const [formData, setformData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+ const {loading,error:errorMessage}= useSelector(state=>state.user);
+  const dispatch=useDispatch;
   const navigate=useNavigate();
   const handleChange = (e) => {
     setformData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -14,11 +15,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if ( !formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all the fields');
+      return dispatch (signInFailure('Please fill out all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+     dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -27,17 +27,16 @@ export default function SignIn() {
 
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.error);
+        dispatch (signInFailure(data.message));
       }
-      setLoading(false);
+    
       if(res.ok)
       {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage("An error occurred during signup.");
-      console.error("Error:", error);
-      setLoading(false);
+      dispatch (signInFailure(error.message));
     }
   };
 
