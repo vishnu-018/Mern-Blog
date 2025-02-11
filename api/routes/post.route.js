f07c50dc71a0ce721a83f6262de40621f96c1aa2
vishnu-018 <vishnu.ec21@bitsathy.ac.in>
2025-02-11 17:16:53 +0530

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,12 +1,12 @@
 
 import express from 'express';
 import { verifyToken } from '../utils/verifiedUser.js';
-import { create } from '../controllers/postcontroller.js';
+import { create, getposts } from '../controllers/postcontroller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
-
+router.get('/getposts', getposts)
 
 
 export default router;
