6594d350a85eab61406774dd198b78e2a1dd5573
vishnu-018 <vishnu.ec21@bitsathy.ac.in>
2025-02-11 16:50:40 +0530

    complete create post functionality

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import postRoutes from  './routes/post.route.js';
+import cors from 'cors';
 dotenv.config();
 
 // Correctly format the MongoDB connection string and call mongoose.connect
@@ -21,6 +22,18 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" })); // Increase JSON payload limit
 app.use(express.urlencoded({ limit: "50mb", extended: true })); // Increase form data limit
+app.use(
+    cors({
+      origin: "http://localhost:5173", // Frontend URL
+      credentials: true,
+    })
+  );
+  
+  app.use((req, res, next) => {
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    next();
+  });
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
