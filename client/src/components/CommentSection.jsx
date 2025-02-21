0b2d0e0b6ce17185310918849d8d9198a00bdcf8
vishnu-018 <vishnu.ec21@bitsathy.ac.in>
2025-02-21 16:40:59 +0530

    add like functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, Textarea } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import Comment from './Comment';
 
 export default function CommentSection({ postId }) {
@@ -10,7 +10,7 @@ export default function CommentSection({ postId }) {
     const [commentError, setCommentError] = useState(null);
     const [comments, setComments] = useState([]);
    
-
+const navigate=useNavigate();
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (comment.length > 200) {
@@ -54,6 +54,34 @@ export default function CommentSection({ postId }) {
         getComments();
       }, [postId]);
     
+
+      const handleLike = async (commentId) => {
+        try {
+          if (!currentUser) {
+            navigate('/sign-in');
+            return;
+          }
+          const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+            method: 'PUT',
+          });
+          if (res.ok) {
+            const data = await res.json();
+            setComments(
+              comments.map((comment) =>
+                comment._id === commentId
+                  ? {
+                      ...comment,
+                      likes: data.likes,
+                      numberOfLikes: data.likes.length,
+                    }
+                  : comment
+              )
+            );
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
 
       return (
         <div className='max-w-2xl mx-auto w-full p-3'>
@@ -123,6 +151,7 @@ export default function CommentSection({ postId }) {
                 <Comment
                   key={comment._id}
                   comment={comment}
+                  onLike={handleLike}
                 />
               ))}
             </>
