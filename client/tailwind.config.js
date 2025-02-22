bfa911a4bb1853178f5277ee8b0fb13efa65d892
vishnu-018 <vishnu.ec21@bitsathy.ac.in>
2025-02-22 10:38:08 +0530

    add recent article section to the post page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 import flowbite from 'flowbite/plugin';
 import tailwindScrollbar from 'tailwind-scrollbar';
+import lineClamp from '@tailwindcss/line-clamp';
+
 export default {
   content: [
-    "./index.html",  // Add this if using Vite
+    "./index.html", // Add this if using Vite
     "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./public/**/*.html",  // Ensure it detects Tailwind classes in public files
+    "./public/**/*.html", // Ensure it detects Tailwind classes in public files
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [flowbite, tailwindScrollbar],
+  plugins: [
+    flowbite,
+    tailwindScrollbar,
+    lineClamp, // Use the imported plugin here
+  ],
 };
