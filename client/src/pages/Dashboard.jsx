7033366dc2d458f0a33d4a6bde4a8d52929c2c46
vishnu-018 <vishnu.ec21@bitsathy.ac.in>
2025-02-23 10:29:46 +0530

    add dashboard overview to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashBoardComp from '../components/DashBoardComp';
 export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState('');
@@ -32,6 +33,8 @@ export default function Dashboard() {
       {tab === 'users' && <DashUsers />}
        {/* comments  */}
        {tab === 'comments' && <DashComments />}
+        {/* dashboard comp */}
+        {tab === 'dash' && <DashBoardComp />}
     </div>
   );
 
